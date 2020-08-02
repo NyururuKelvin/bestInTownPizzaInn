cc8ec689d63df1fd07e45817355837860ef31f4c
Nyururukelvin <nyururukelvin9@gmail.com>
2020-08-02 10:19:40 +0300

    my commit add order function and local variables

--- js/script.js
+++ js/script.js
@@ -17,6 +17,21 @@
 $(document).ready(function(){
     $('#submit').click(function(event){
         event.preventDefault();
-        var type = $
+        var type = $('#type').val();
+        var size = parseInt($("input[name='size']:checked").val());
+        var crust = parseInt($("input[name='crust']:checked").val());
+        var amount = parseInt($("#number").val());
+        var price=[200, 500, 800,1000];
+
+        var sumToppings=0;
+
+        $("input[name='topping']:checked").each(function(){
+          sumToppings+=parseInt($(this).val());
     })
+
+    var subTotal = ((price[size]) + crust + sumToppings)* amount;
+
+    if (size =='undefined'|| (price[size]) == 'undefined' || crust == 'NaN' || sumToppings == '' || subTotal == 'NaN'){         
+        $("#subtotal").html('Kindly select at least one size, and a crust.')
+    } else
 })
