5d6f603a6d076ab602c97abb53b7b2066eef81f5
Nyururukelvin <nyururukelvin9@gmail.com>
2020-08-02 09:46:40 +0300

    my commit add getSize function

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// getSize function
+ 
+ function getSize (size) {
+    if (size === 0) {
+        return "Pizza Pie"
+   }else if (size === 1) {
+        return "Regular"
+    }else if (size === 2) {
+        return "Medium"
+    }else if (size === 3) {
+     return "large"
+   }
+ };
